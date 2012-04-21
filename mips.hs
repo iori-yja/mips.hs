55087b739011ad24a5b9484bd2aebdd45f66197e
iori-y <iori@switch-science.com>
2012-04-22 01:49:54 +0900

    I-instruction and R-instraction is inplemented but shift, special, multiplex. BIG ERATTA: Cant detect underflow.

--- mips.hs
+++ mips.hs
@@ -43,13 +43,16 @@ exc (Set (sr,rs,mem) , Meta "fill") | rs !! 0 == 0  = Running $ Set (sr, fillout
 exc (Set (sr,rs,mem) , Meta "hlt")                  = Interrupt $ Set (sr, fillout 0 32,mem)
 exc (context , Meta "mem")                      = Interrupt context
 
-exc (st , Rinst "addu" d s t _)        = ctrl (+)   True  Reg st d s t
-exc (st , Rinst "subu" d s t _)        = ctrl (-)   True  Reg st d s t
 exc (st , Rinst "add" d s t _)         = ctrl (+)   False Reg st d s t
+exc (st , Rinst "addu" d s t _)        = ctrl (+)   True  Reg st d s t
 exc (st , Rinst "sub" d s t _)         = ctrl (-)   False Reg st d s t
-exc (st , Rinst "xor" d s t _)         = ctrl xor   False Reg st d s t
-exc (st , Rinst "and" d s t _)         = ctrl (.&.) False Reg st d s t
-exc (st , Rinst "or" d s t _)          = ctrl (.|.) False Reg st d s t
+exc (st , Rinst "subu" d s t _)        = ctrl (-)   True  Reg st d s t
+exc (st , Rinst "slt" d s t _)         = ctrl (\x y -> if ( x < y ) then 1 else 0 ) True Reg st d s t
+exc (st , Rinst "sltu" d s t _)        = ctrl (\x y -> if ( x < y ) then 1 else 0 ) True Reg st d s t
+exc (st , Rinst "and" d s t _)         = ctrl (.&.) True Reg st d s t
+exc (st , Rinst "or" d s t _)          = ctrl (.|.) True Reg st d s t
+exc (st , Rinst "nor" d s t _)         = ctrl (\x y -> xor 0xffffffff $ x .|. y ) True Reg st d s t
+exc (st , Rinst "xor" d s t _)         = ctrl xor   True Reg st d s t
 
 exc (st , Rinst "sll" d _ t q)         = ctrl shiftL True Imm st d t q
 exc (st , Rinst "sllv" d s t _)        = ctrl shiftL True Reg st d s t
@@ -60,19 +63,34 @@ exc (st , Rinst "sra" d _ t q)         = ctrl (\x y -> if ( testBit x 31 )
 exc (st , Rinst "srav" d s _ q)        = ctrl (\x y -> if ( testBit x 31 )
    						     then foldl setBit (x `shiftR` y) [(32-y)..31] else x `shiftR` y)
 					 True Imm st d s q
-exc (st , Rinst "srlv" d s _ q)        = ctrl shiftR True  Imm st d s q
-exc (st , Iinst "addi" d s im)         = ctrl (+)    False Imm st d s im
-exc (st , Iinst "subi" d s im)         = ctrl (-)    False Imm st d s im
+exc (st , Rinst "srlv" t s _ q)        = ctrl shiftR True  Imm st t s q
+exc (st , Iinst "addi" t s im)         = ctrl (+)    False Imm st t s im
+exc (st , Iinst "subi" t s im)         = ctrl (-)    False Imm st t s im
+exc (st , Iinst "addiu" t s im)        = ctrl (+)    True  Imm st t s im
+exc (st , Iinst "subiu" t s im)        = ctrl (-)    True  Imm st t s im
 
-exc (st@(Set (sr,rs,mem)), Iinst "sw" b r o) = Running $ Set (sr, rs, M.insert ((rs !! b) + o) (rs !! r) mem)
-exc (st@(Set (sr,rs,mem)), Iinst "lw" b r o) = let p = memlook ( (rs !! b) + o ) mem
-						   rg = ( take b rs ) ++ p `mod` (1 `shift` 32) : drop (b+1) rs
-					       in  Running $ Set (sr, rg, mem)
+exc (st , Iinst "slti" t s im)         = ctrl (\x y -> if ( x < y ) then 1 else 0 ) True Imm st t s im
+exc (st , Iinst "sltiu" t s im)        = ctrl (\x y -> if ( x < y ) then 1 else 0 ) True Imm st t s im
+
+exc (st , Iinst "andi" t s im)         = ctrl (.&.) True Imm st t s ( im .&. 0xffff )
+exc (st , Iinst "ori" t s im)          = ctrl (.|.) True Imm st t s ( im .&. 0xffff )
+exc (st , Iinst "xori" t s im )        = ctrl xor   True Imm st t s ( im .&. 0xffff )
+exc (st , Iinst "lui" t s im )         = ctrl (\x _-> x `shiftL` 16 ) True Imm st t s ( im .&. 0xffff )
+
+exc (st@(Set (sr,rs,mem)), Iinst "sw" r b o) = let addr = ( (rs !! b) + o )
+					       in  case ( addr `mod` 4 ) of
+							0 -> Running $ Set (sr, rs, M.insert ((rs !! b) + o) (rs !! r) mem)
+							_ -> Interrupt st
+exc (st@(Set (sr,rs,mem)), Iinst "lw" r b o) = let addr = ( (rs !! b) + o )
+						   p = memlook addr mem
+						   rg = ( take r rs ) ++ p `mod` (1 `shift` 32) : drop (r+1) rs
+					       in  case ( addr `mod` 4 ) of
+							0 -> Running $ Set (sr, rg, mem)
+							_ -> Interrupt $ Set (sr, rs, mem)
 					       where
 					           memlook x m = case (M.lookup x m) of
 								      Just a -> a
 								      Nothing -> 0
-
 
 exc (st@(Set ( _ , a , _ ) , _) )      = Interrupt $ Set ( ( 0 , 0 , 0 ) , a , M.empty)
 
