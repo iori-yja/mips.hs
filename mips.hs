a9becf5a3b768e20e7d699e0cdf4392a476d030d
iori-y <iori@switch-science.com>
2012-04-22 00:38:26 +0900

    Load and store is works. Memory is represented with a dictionary.

--- mips.hs
+++ mips.hs
@@ -1,4 +1,5 @@
 import Data.Bits
+import qualified Data.Map as M
 import Numeric
 --mips primary emuration
 
@@ -8,13 +9,12 @@ data Instraction = Rinst [Char] Int Int Int Int
 		 | Meta [Char]
 	deriving (Eq, Show, Read)
 
-newtype Set = Set ((Int,Int,Int),[Int],[Int])
-	deriving (Eq, Show, Read)
-data Status = Interrupt Set | Running Set
-	deriving (Eq, Show, Read)
+newtype Set = Set ((Int,Int,Int),[Int],M.Map Int Int) deriving (Eq, Show, Read)
+data Status = Interrupt Set | Running Set deriving (Eq, Show, Read)
+data SndOp = Imm | Reg deriving (Eq, Show, Read)
 
 initenv :: IO ()
-initenv = runenv $ Set ((0,0,0),(fillout 0 32),[])
+initenv = runenv $ Set ((0,0,0),(fillout 0 32),M.empty)
 
 fillout :: Int -> Int -> [Int]
 fillout _ 0 = []
@@ -26,8 +26,9 @@ runenv rs@(Set (sr,rg,m)) = do
 	putStrLn ""
 	x <- getLine
 	case (exc (rs, fetch x)) of
-	     Interrupt y -> do
+	     Interrupt y@(Set (_,_,mem)) -> do
 		      putStrLn "Interrupted"
+		      putStr $ M.showTree mem
 		      runenv y
 	     Running y -> runenv y
 	where
@@ -40,38 +41,45 @@ exc :: (Set, Instraction) -> Status
 exc (Set (sr,rs,mem) , Meta "clear") | rs !! 0 == 1 = Running $ Set (sr, fillout 0 32,mem)
 exc (Set (sr,rs,mem) , Meta "fill") | rs !! 0 == 0  = Running $ Set (sr, fillout 1 32,mem)
 exc (Set (sr,rs,mem) , Meta "hlt")                  = Interrupt $ Set (sr, fillout 0 32,mem)
+exc (context , Meta "mem")                      = Interrupt context
 
-exc (st , Rinst "addu" d s t _)        = rctrl (+) True st d s t
-exc (st , Rinst "subu" d s t _)        = rctrl (-) True st d s t
-exc (st , Rinst "add" d s t _)         = rctrl (+) False st d s t
-exc (st , Rinst "sub" d s t _)         = rctrl (-) False st d s t
-exc (st , Rinst "xor" d s t _)         = rctrl xor False st d s t
-exc (st , Rinst "and" d s t _)         = rctrl (.&.) False st d s t
-exc (st , Rinst "or" d s t _)          = rctrl (.|.) False st d s t
+exc (st , Rinst "addu" d s t _)        = ctrl (+)   True  Reg st d s t
+exc (st , Rinst "subu" d s t _)        = ctrl (-)   True  Reg st d s t
+exc (st , Rinst "add" d s t _)         = ctrl (+)   False Reg st d s t
+exc (st , Rinst "sub" d s t _)         = ctrl (-)   False Reg st d s t
+exc (st , Rinst "xor" d s t _)         = ctrl xor   False Reg st d s t
+exc (st , Rinst "and" d s t _)         = ctrl (.&.) False Reg st d s t
+exc (st , Rinst "or" d s t _)          = ctrl (.|.) False Reg st d s t
 
-exc (st , Rinst "sll" d _ t q)         = ictrl shiftL True st d t q
-exc (st , Rinst "sllv" d s t _)        = rctrl shiftL True st d s t
-exc (st , Rinst "srl" d _ t q)         = ictrl shiftR True st d t q
-exc (st , Rinst "sra" d _ t q)         = ictrl (\x y -> if ( testBit x 31 )
+exc (st , Rinst "sll" d _ t q)         = ctrl shiftL True Imm st d t q
+exc (st , Rinst "sllv" d s t _)        = ctrl shiftL True Reg st d s t
+exc (st , Rinst "srl" d _ t q)         = ctrl shiftR True Imm st d t q
+exc (st , Rinst "sra" d _ t q)         = ctrl (\x y -> if ( testBit x 31 )
    						     then foldl setBit (x `shiftR` y) [(32-y)..31] else x `shiftR` y)
-   					 True st d t q
-exc (st , Rinst "srav" d s _ q)        = ictrl shiftR True st d s q
-exc (st , Rinst "srlv" d s _ q)        = ictrl shiftR True st d s q
-exc (st , Iinst "addi" d s im)         = ictrl (+) False st d s im
-exc (st , Iinst "subi" d s im)         = ictrl (-) False st d s im
-exc (st , _)                           = Interrupt $ Set ((0,0,0),[],[])
+   					 True Imm st d t q
+exc (st , Rinst "srav" d s _ q)        = ctrl (\x y -> if ( testBit x 31 )
+   						     then foldl setBit (x `shiftR` y) [(32-y)..31] else x `shiftR` y)
+					 True Imm st d s q
+exc (st , Rinst "srlv" d s _ q)        = ctrl shiftR True  Imm st d s q
+exc (st , Iinst "addi" d s im)         = ctrl (+)    False Imm st d s im
+exc (st , Iinst "subi" d s im)         = ctrl (-)    False Imm st d s im
 
-rctrl :: (Int -> Int -> Int) -> Bool -> Set -> Int -> Int -> Int -> Status
-rctrl f u st@(Set(sr,rs,mem)) d s t  = let p = f (rs !! s) (rs !! t)
-		                           rg = ( take d rs ) ++ p `mod` (1 `shift` 32) : drop (d+1) rs
-					   stn = Set (sr, rg, mem)
-	              in if p < (1 `shift` 32) || u then Running stn
-						    else Interrupt stn
+exc (st@(Set (sr,rs,mem)), Iinst "sw" b r o) = Running $ Set (sr, rs, M.insert ((rs !! b) + o) (rs !! r) mem)
+exc (st@(Set (sr,rs,mem)), Iinst "lw" b r o) = let p = memlook ( (rs !! b) + o ) mem
+						   rg = ( take b rs ) ++ p `mod` (1 `shift` 32) : drop (b+1) rs
+					       in  Running $ Set (sr, rg, mem)
+					       where
+					           memlook x m = case (M.lookup x m) of
+								      Just a -> a
+								      Nothing -> 0
 
-ictrl :: (Int -> Int -> Int) -> Bool -> Set -> Int -> Int -> Int -> Status
-ictrl f u st@(Set(sr,rs,mem)) d s im  = let p = f (rs !! s) im
+
+exc (st@(Set ( _ , a , _ ) , _) )      = Interrupt $ Set ( ( 0 , 0 , 0 ) , a , M.empty)
+
+ctrl :: (Int -> Int -> Int) -> Bool -> SndOp -> Set -> Int -> Int -> Int -> Status
+ctrl f u i st@(Set(sr,rs,mem)) d s t  = let p = f (rs !! s) (if i == Reg then (rs !! t) else t)
 		                            rg = ( take d rs ) ++ p `mod` (1 `shift` 32) : drop (d+1) rs
-		 			    stn = Set (sr, rg, mem)
+					    stn = Set (sr, rg, mem)
 	              in if p < (1 `shift` 32) || u then Running stn
 						    else Interrupt stn
 
